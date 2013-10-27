eb4effc2cfb5561ef11a61cd3fc771f8c948e612
Alexandre Patry <alexandre.patry@nlpfu.com>
2013-10-26 22:03:38 -0400

    Add an example migration script

--- src/migrations/001_base_schema.clj
+++ src/migrations/001_base_schema.clj
@@ -1,5 +1,20 @@
-(ns migrations.001-base-schema)
+(ns migrations.001-base-schema
+  (:use [korma core db]))
 
-(defn up [])
+(defn up []
+  (transaction
+   (exec-raw (str "CREATE TABLE author ("
+                  "id INT PRIMARY KEY,"
+                  "firstname TEXT NOT NULL,"
+                  "lasname TEXT NOT NULL"
+                  ")"))
+   (exec-raw (str "CREATE TABLE book ("
+                  "id INT PRIMARY KEY,"
+                  "title TEXT NOT NULL,"
+                  "author_id INT REFERENCES author(id)"
+                  ")"))))
 
-(defn down [])
+(defn down []
+  (transaction
+   (exec-raw "DROP TABLE book")
+   (exec-raw "DROP TABLE author")))
